7875c4b1b792777cec8b8c5bb1224a82f6003bfe
MahmoudAbdelFatah <abdelfatahm3@gmail.com>
2018-03-29 02:22:08 +0200

    add logo and Instabug library

---
+++ app/src/main/java/com/example/wasla/util/MyConstants.java
@@ -0,0 +1,13 @@
+package com.example.wasla.util;
+
+import android.os.Build;
+
+import com.example.wasla.BuildConfig;
+
+/**
+ * Created by mahmoudabdelfatahabd on 29-Mar-18.
+ */
+
+public class MyConstants {
+    public static final String INSTABUG_KEY = BuildConfig.INSTABUG_KEY;
+}
